6f97c7169c47d7eab06902005ef073d91c28d258
devthigas502 <thiagodev502@gmail.com>
2024-05-07 15:22:52 -0300

    setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-600">
+      <Text className="text-white">index</Text>
+    </View>
+  )
+}
